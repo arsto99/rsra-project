d4ad2a819783f48b0d19a20de355574c3a2dcb73
rsto99 <r.arsto99@gmail.com>
2025-01-01 02:28:10 +0300

    Switch to BrowserRouter with correct basename

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/rsra-project">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
