cf37277acbafefe687bc346d707b8f9af9d40a6b
rsto99 <r.arsto99@gmail.com>
2025-01-01 04:16:20 +0300

    إصلاح مشكلة المسارات غير الموجودة

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,16 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/auth/Login'
 import Register from './pages/auth/Register'
 import ForgotPassword from './pages/auth/ForgotPassword'
-import ResetPassword from './pages/auth/ResetPassword'
-import EmailVerification from './pages/auth/EmailVerification'
 import './index.css'
 
 function App() {
   return (
-    <div className="font-cairo" dir="rtl">
+    <div className="font-cairo">
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password/:token" element={<ResetPassword />} />
-        <Route path="/verify-email/:token" element={<EmailVerification />} />
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </div>
