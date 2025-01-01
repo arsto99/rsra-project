42ecb217ea86a5ce550c04b6f4f9647097fb8b6e
rsto99 <r.arsto99@gmail.com>
2025-01-01 04:24:39 +0300

    إضافة صفحة التحقق من البريد الإلكتروني

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Login from './pages/auth/Login'
 import Register from './pages/auth/Register'
 import ForgotPassword from './pages/auth/ForgotPassword'
 import ResetPassword from './pages/auth/ResetPassword'
+import EmailVerification from './pages/auth/EmailVerification'
 import './index.css'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password/:token" element={<ResetPassword />} />
+        <Route path="/verify-email/:token" element={<EmailVerification />} />
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </div>
